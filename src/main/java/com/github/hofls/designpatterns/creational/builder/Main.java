af67c2c8c18ddab596c04619bc9f0aa694e90505
Hofls <pleshakov@proitr.ru>
2020-03-12 18:18:25 +0500

    Builder

---
+++ src/main/java/com/github/hofls/designpatterns/creational/builder/Main.java
@@ -0,0 +1,21 @@
+package com.github.hofls.designpatterns.creational.builder;
+
+/**
+ * Builder lets you construct complex objects step by step
+ */
+public class Main {
+
+    public void onStartup() {
+
+        DocumentParts.Document document =
+                new DocumentBuilder()
+                .addHeader(new DocumentParts.Element())
+                .addBody(new DocumentParts.Element())
+                .addFooter(new DocumentParts.Element())
+                .build();
+
+    }
+
+
+
+}
